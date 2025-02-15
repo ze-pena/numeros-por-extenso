06819409cb818d4ac3a8e82a1e861ba67f43d576
ze-pena <jose.pena.dev@gmail.com>
2025-02-15 20:17:00 -0300

    feat: create a file to build main page elements

---
+++ scripts/builders/converter.js
@@ -0,0 +1,62 @@
+import { handleInput } from "../events/input.js"
+
+function buildAppName() {
+  const container = document.createElement("div");
+  const title = document.createElement("h1");
+  const icon = document.createElement("div");
+
+  container.classList.add("name");
+  title.classList.add("name-title");
+  icon.classList.add("name-icon");
+
+  title.innerText = "Números por extenso";
+  container.appendChild(title);
+  container.appendChild(icon);
+  return container;
+}
+
+function buildAppInput() {
+  const container = document.createElement("div");
+  const label = document.createElement("label");
+  const icon = document.createElement("div");
+  const input = document.createElement("input");
+
+  container.classList.add("input");
+  label.classList.add("input-label");
+  icon.classList.add("label-icon");
+  input.classList.add("label-field");
+
+  input.setAttribute("title", "number-input");
+  input.setAttribute("type", "text");
+  input.setAttribute("placeholder", "Insira o número desejado.");
+  input.addEventListener("input", handleInput);
+
+  label.appendChild(icon);
+  label.appendChild(input);
+  container.appendChild(label);
+  return container;
+}
+
+function buildAppOutput() {
+  const container = document.createElement("div");
+  const output = document.createElement("p");
+
+  container.classList.add("output");
+  output.classList.add("output-field");
+
+  container.appendChild(output);
+  return container;
+}
+
+export default function buildAppConverter() {
+  const container = document.createElement("main");
+  const appName = buildAppName();
+  const appInput = buildAppInput();
+  const appOutput = buildAppOutput();
+
+  container.classList.add("app-converter")
+  container.appendChild(appName);
+  container.appendChild(appInput);
+  container.appendChild(appOutput);
+  return container;
+}
