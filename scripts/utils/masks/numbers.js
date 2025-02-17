f73f5e96491aaeb5e32cbeddea429b10978d2298
ze-pena <jose.pena.dev@gmail.com>
2025-02-17 15:40:39 -0300

    refactor: move masks file to another directory

---
+++ scripts/utils/masks/numbers.js
@@ -0,0 +1,26 @@
+export function fractionNumberMask(value) {
+  if (!value || (typeof value !== "string" && typeof value !== "number")) return value;
+
+  const formatOptions = { style: "currency", currency: "BRL", maximumFractionDigits: 2, useGrouping: false };
+  let formatter = Intl.NumberFormat("pt-BR", formatOptions);
+  let newValue = 0;
+
+  newValue = typeof value === "string" ? Number(value.slice(0, 4).replace(/\D/g, "")) : value;
+
+  return String(formatter.format(newValue / 100)).replace("R$", "").trim();
+}
+
+export function integerNumberMask(value) {
+  if (!value || (typeof value !== "string" && typeof value !== "number")) return value;
+
+  let newValue = typeof value === "number" ? String(value) : value;
+
+  if (isNaN(newValue)) return value;
+
+  newValue = newValue
+    .slice(0, 4)
+    .replace(/\D/g, "")
+    .replace(/^([0]+)([\d])/gi, "$2");
+
+  return newValue;
+}
